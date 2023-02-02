274c9e089ea9427109ce2d75cc3dfc39bb68d7dc
aditibansalu <118024666+aditibansalu@users.noreply.github.com>
2023-02-02 00:14:21 -0500

    Initial Commit

---
+++ H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by Aditi Bansal on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
